90670c944d74839a1625d0664c5ce9ba243ad71e
Shelby Kohring <shelbykohring@Shelbys-MacBook-Pro-2.local>
2020-11-05 23:20:42 -0600

    link index.html and single-repo.html dynamically/error handling

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,20 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function () {
+  // grab repo name from url query string
+  var queryString = document.location.search;
+  var repoName = queryString.split("=")[1];
+  if (repoName) {
+    // display repo name on the page
+    repoNameEl.textContent = repoName;
+    getRepoIssues(repoName);
+  } else {
+    // if no repo was given redirect to the homepage
+    document.location.replace("./index.html");
+  }
+};
 
 var getRepoIssues = function (repo) {
   var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -16,12 +31,10 @@ var getRepoIssues = function (repo) {
         }
       });
     } else {
-      alert("There was a problem with your request!");
+      document.location.replace("./index.html");
     }
   });
 };
-
-getRepoIssues("facebook/react");
 
 var displayIssues = function (issues) {
   if (issues.length === 0) {
@@ -65,7 +78,7 @@ var displayWarning = function (repo) {
 
   // create element to display link to githug repo issues exceeding 30 issues
   var linkEl = document.createElement("a");
-  
+
   linkEl.textContent = "view this repository at GitHub.com";
   linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
   linkEl.setAttribute("target", "_blank");
@@ -73,3 +86,5 @@ var displayWarning = function (repo) {
   // append to warningcontainer
   limitWarningEl.appendChild(linkEl);
 };
+
+getRepoName();
